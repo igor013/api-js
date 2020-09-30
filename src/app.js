94dc396efc5429b542a011de6e67731f7f3dcd8e
hog099 <oliveiraginelli@gmail.com>
2020-09-30 19:45:19 -0300

    middleware rota invalida

--- src/app.js
+++ src/app.js
@@ -9,4 +9,9 @@ app.use(cors());
 
 app.use(routes);
 
+//Implementacao de Middleware para Rotas Invalidas
+    app.use(function (req, res, next) {
+        res.status(404).send("Não encontrado a rota digitada!")
+    })
+
 export default app;
