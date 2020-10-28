50bd150c78a896bbbcacf3b6906c89a03f6d0e28
igorhenriqueml <igorhenriqueml@gmail.com>
2020-10-27 22:03:09 -0300

    aula front 2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
+import "dotenv/config";
 import cors from "cors";
 import routes from "./routes.js";
-import "./database/index"
+import "./database/index";
 
 
 class App {
--- src/app/controlles/SessionController.js
+++ src/app/controlles/SessionController.js
@@ -1,29 +1,36 @@
-// import Sequelize from "sequelize";
+import Jwt from 'jsonwebtoken';
+import authConf from '../../config/Auth';
 import User from "../models/User.js";
 class SessionController {
     async store(req, res) {
         const { mail, password } = req.body;
-        //    console.log(mail)
-try {
-    const fulano = await User.findOne({
-        // attributes: ['id', 'name', 'mail', 'age', 'status'],
-        where: {
-            mail: mail
+        console.log("cheguei aquiiiiiiiiii,",req.body);
+        try {
+            const fulano = await User.findOne({
+                // attributes: ['id', 'name', 'mail', 'age', 'status'],
+                where: {
+                    mail: mail
+                }
+               
+            });
+            if (!fulano) { return res.status(400).json({ message: "email nao encontrado" }) }
+            
+            let isvalid = await fulano.checkPassword(password);
+            
+            if (!isvalid) {
+                return res.status(400).json({ message: "senha invalida" })
+            }
+            
+            // console.log("LOGGGGGG", authConf)
+
+            const {id} = fulano;
+            let token = Jwt.sign({id}, authConf.secret ,{expiresIn:authConf.expiresIn});
+            return res.status(200).json({user:fulano,token:token});
+
+        } catch (error) {
+            console.log("error", error)
+
         }
-    });
-    if (!fulano) { return res.status(400).json({ message: "email nao encontrado" }) }
-    let isvalid = await fulano.checkPassword(password);
-    
-    if (!isvalid) {
-        return res.status(400).json({ message: "senha invalida" })
-    }
-
-    return res.json(fulano)
-
-} catch (error) {
-    console.log("error", error)
-    
-}
     }
 
 
